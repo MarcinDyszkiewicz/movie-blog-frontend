51e242849e58717d144afbbddc8335981b9e8d08
marcin <mdyszkiewicz@9bits.pl>
2019-01-05 15:35:17 +0100

    Added Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer>
+        <div class="flex items-center justify-between flex-wrap bg-black p-2">
+
+        </div>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: "footer.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
